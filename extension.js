22cc815e96b2fa99aecde63e3bb85e98c2e3e8da
themicrocheeze <themicrocheez@gmail.com>
2022-01-10 14:19:50 -0500

    Updated Team Name Tag Colors
    The nameTag elements no longer use lightblue and firebrick, they have been switched to 375c79 and 9f2e2e respectively

--- extension.js
+++ extension.js
@@ -1,6 +1,5 @@
 'use strict';
 
 module.exports = function (nodecg) {
-	nodecg.log.info('ESSENTIAL SCORE VERSION 1.0')
-	nodecg.log.info(`To edit me, open "${__filename}" in your favorite text editor or IDE.`);
+	nodecg.log.info('ESSENTIAL SCORE VERSION 0.0.6')
 };
